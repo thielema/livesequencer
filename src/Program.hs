812ddaede08e633b166f6b4e34b13a131a79169d
Henning Thielemann <git@henning-thielemann.de>
2011-10-15 18:34:20 +0200

    Program: remove fields functions, constructors because they are currently unused

--- src/Program.hs
+++ src/Program.hs
@@ -1,23 +1,26 @@
 module Program where
 
 import IO
-import Term
+-- import Term
 import Rule
 
 import Text.Parsec
 import Text.PrettyPrint.HughesPJ
+{-
 import qualified Data.Set as S
 import Control.Monad ( guard )
+-}
 
 data Program = Program
                { rules :: [ Rule ]
-               , functions :: S.Set Identifier
-               , constructors :: S.Set Identifier
+               -- , functions :: S.Set Identifier
+               -- , constructors :: S.Set Identifier
                }
 
 instance Input Program where
   input = do
     rs <- many input
+{-
     let fs = S.fromList $ do
           r <- rs
           let Node f _xs = lhs r
@@ -28,9 +31,10 @@ instance Input Program where
           guard $ isConstructor f
           return f
     return $ Program { rules = rs, functions = fs, constructors = cs }
+-}
+    return $ Program { rules = rs }
 instance Output Program where
   output p = vcat $ map output $ rules p
 
 instance Show Program where show = render . output
 instance Read Program where readsPrec = parsec_reader
-  
