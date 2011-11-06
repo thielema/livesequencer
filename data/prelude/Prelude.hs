6cb41107f9a52576a0f5f4985606c81811583d28
Henning Thielemann <git@henning-thielemann.de>
2011-11-06 12:03:50 +0100

    data/Prelude.min, abs, signum

--- data/prelude/Prelude.hs
+++ data/prelude/Prelude.hs
@@ -1,5 +1,18 @@
 module Prelude where
 
+import Bool
+
+
 data Ordering = LT | EQ | GT ;
+
+signumFromOrdering LT = 0-1 ;
+signumFromOrdering EQ = 0 ;
+signumFromOrdering GT = 1 ;
+
+signum x = signumFromOrdering (compare x 0) ;
+
+abs x = signum x * x ;
+
+min x y = ifThenElse (x<y) x y ;
 
 negate x = 0 - x ;
