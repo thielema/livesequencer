d5b5da5228c5ec61ccf33a246234714e1c0ef5b0
Henning Thielemann <git@henning-thielemann.de>
2011-12-11 13:25:31 +0100

    Pitch: allow note names like cis and des

--- data/Pitch.hs
+++ data/Pitch.hs
@@ -6,10 +6,18 @@ http://en.wikipedia.org/wiki/MIDI_Tuning_Standard
 -}
 pitch cls octave = cls + (octave+1)*12 ;
 
-cb =  0 ; c = pitch cb ; cs = pitch (cb+1) ; cf = pitch (cb-1) ;
-db =  2 ; d = pitch db ; ds = pitch (db+1) ; df = pitch (db-1) ;
-eb =  4 ; e = pitch eb ; es = pitch (eb+1) ; ef = pitch (eb-1) ;
-fb =  5 ; f = pitch fb ; fs = pitch (fb+1) ; ff = pitch (fb-1) ;
-gb =  7 ; g = pitch gb ; gs = pitch (gb+1) ; gf = pitch (gb-1) ;
-ab =  9 ; a = pitch ab ; as = pitch (ab+1) ; af = pitch (ab-1) ;
-bb = 11 ; b = pitch bb ; bs = pitch (bb+1) ; bf = pitch (bb-1) ;
+cb =  0 ; c = pitch cb ; cis = cs ; ces = cf ;
+db =  2 ; d = pitch db ; dis = ds ; des = df ;
+eb =  4 ; e = pitch eb ; eis = es ; ees = ef ;
+fb =  5 ; f = pitch fb ; fis = fs ; fes = ff ;
+gb =  7 ; g = pitch gb ; gis = gs ; ges = gf ;
+ab =  9 ; a = pitch ab ; ais = as ; aes = af ;
+bb = 11 ; b = pitch bb ; bis = bs ; bes = bf ;
+
+cs = pitch (cb+1) ; cf = pitch (cb-1) ;
+ds = pitch (db+1) ; df = pitch (db-1) ;
+es = pitch (eb+1) ; ef = pitch (eb-1) ;
+fs = pitch (fb+1) ; ff = pitch (fb-1) ;
+gs = pitch (gb+1) ; gf = pitch (gb-1) ;
+as = pitch (ab+1) ; af = pitch (ab-1) ;
+bs = pitch (bb+1) ; bf = pitch (bb-1) ;
