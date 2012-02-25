420d1778893c5cc3e9a8a842c152ed11c6463baf
Henning Thielemann <git@henning-thielemann.de>
2012-02-25 23:37:06 +0100

    data/CrossSum: algorithmic note sequence example

---
+++ data/CrossSum.hs
@@ -0,0 +1,60 @@
+module CrossSum where
+
+import Midi ;
+import List ;
+import Pitch ;
+import Prelude ( Integer, ($), (.), (+), mod, div, succ ) ;
+
+
+main :: [ Event (Channel Message) ] ;
+main =
+   channel 0 $
+   concatMap (note qn . makePitch) $
+   modCrossSums 3 ;
+
+makePitch :: Integer -> Pitch ;
+makePitch 0 = c 4 ;
+makePitch 1 = e 4 ;
+makePitch 2 = g 4 ;
+makePitch _ = c 5 ;
+
+
+-- * explicit base conversion
+
+modCrossSums :: Integer -> [Integer] ;
+modCrossSums n =
+   map (foldl (modAdd n) 0 . toBase n) $
+   iterateInteger succ 0 ;
+
+toBase :: Integer -> Integer -> [Integer] ;
+toBase _n 0 = [] ;
+toBase n x = mod x n : toBase n (div x n) ;
+
+
+-- * recursion on lists
+
+modCrossSumsList :: Integer -> [Integer] ;
+modCrossSumsList n =
+   0 : extList n [0] ;
+
+extList :: Integer -> [Integer] -> [Integer] ;
+extList n y =
+   extList2 n $ map (incList n y) $
+   take n $ iterateInteger succ 0 ;
+
+extList2 :: Integer -> [[Integer]] -> [Integer] ;
+extList2 n z =
+   concat (tail z) ++ extList n (concat z) ;
+
+incList :: Integer -> [Integer] -> Integer -> [Integer] ;
+incList m ys x = map (modAdd m x) ys ;
+
+
+-- * auxiliary
+
+modAdd :: Integer -> Integer -> Integer -> Integer ;
+modAdd m x y = mod (x+y) m ;
+
+
+qn :: Integer ;
+qn = 150 ;
