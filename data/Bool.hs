6809dcb1e6ad43d38e60eb885e4a970b8725d772
Henning Thielemann <git@henning-thielemann.de>
2012-02-25 23:56:19 +0100

    data/Bool: infix declarations

--- data/Bool.hs
+++ data/Bool.hs
@@ -4,13 +4,20 @@ ifThenElse :: Bool -> a -> a -> a ;
 ifThenElse True  y _ = y ;
 ifThenElse False _ n = n ;
 
+
 not :: Bool -> Bool ;
 not False = True ;
 not True = False ;
 
+
+infixr 3 && ;
+
 (&&) :: Bool -> Bool -> Bool ;
 False && _ = False ;
 True && x = x ;
+
+
+infixr 2 || ;
 
 (||) :: Bool -> Bool -> Bool ;
 True || _ = True ;
