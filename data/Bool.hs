52048e66a500059451c0c20cc0b0e20df0da2a64
Henning Thielemann <git@henning-thielemann.de>
2012-02-25 23:29:52 +0100

    data/Bool: not, &&, ||

--- data/Bool.hs
+++ data/Bool.hs
@@ -3,3 +3,15 @@ module Bool where
 ifThenElse :: Bool -> a -> a -> a ;
 ifThenElse True  y _ = y ;
 ifThenElse False _ n = n ;
+
+not :: Bool -> Bool ;
+not False = True ;
+not True = False ;
+
+(&&) :: Bool -> Bool -> Bool ;
+False && _ = False ;
+True && x = x ;
+
+(||) :: Bool -> Bool -> Bool ;
+True || _ = True ;
+False || x = x ;
