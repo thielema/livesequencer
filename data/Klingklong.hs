db43148a9cff707db1568df20836307829860057
Henning Thielemann <git@henning-thielemann.de>
2012-02-26 12:17:07 +0100

    data/Klingklong: type signatures

--- data/Klingklong.hs
+++ data/Klingklong.hs
@@ -4,14 +4,17 @@ import Chords
 import Pitch
 import Midi
 import List
-import Prelude ( (*), (+), (-) )
+import Prelude ( Integer, (*), (+), (-) )
 
 
+main :: [Midi.Event (Midi.Channel Midi.Message)] ;
 main = [] ;
 
 
 -- * patterns
 
+loop0, loop0_1, loop1, loop2a, loop2b
+   :: [Midi.Event (Midi.Channel Midi.Message)] ;
 loop0 = patternChannel ( note qn (c 4) ) ++ loop0 ;
 
 loop0_1 =
@@ -20,6 +23,8 @@ loop0_1 =
     ) ++ loop0_1 ;
 
 
+pattern1, pattern2a, pattern2b, pattern3a, pattern3b
+   :: [Midi.Event (Midi.Channel Midi.Message)] ;
 pattern1 = patternChannel ( concat [
   note qn (e 4), note qn (c 4),
   note qn (e 4), note qn (c 4),
@@ -27,6 +32,7 @@ pattern1 = patternChannel ( concat [
   note en (d 4), note en (e 4), note en (f 4),
     note en (d 4) ] ) ;
 
+pattern2 :: [Midi.Event Midi.Message] ;
 pattern2 = concat [
   note qn (e 4), note qn (a 4),
   note qn (e 4), note qn (a 4),
@@ -34,10 +40,10 @@ pattern2 = concat [
   note en (d 5), note en (c 5), note en (b 4) ] ;
 
 pattern2a =
-  patternChannel ( concat [pattern2, note en (a 4) ] ) ;
+  patternChannel ( pattern2 ++ note en (a 4) ) ;
 
 pattern2b =
-  patternChannel ( concat [pattern2, note en (g 4) ] ) ;
+  patternChannel ( pattern2 ++ note en (g 4) ) ;
 
 
 pattern3a = patternChannel ( concat [
@@ -63,6 +69,15 @@ loop2b = pattern2b ++ loop1 ;
 
 
 -- * pad
+
+padSimple, pad1, pad2, pad3,
+  bass3, bassLoop,
+  sweep, loop1p,
+  pad3loop, loop3sweep, bassPad,
+  pattern1Bass, pattern1BassPadShort,
+  pattern1BassPad, pattern2BassPad, pattern3BassPad
+    :: [Midi.Event (Midi.Channel Midi.Message)] ;
+
 
 padSimple = padChannel ( concat [
   pad,
@@ -102,10 +117,12 @@ pad3 = padChannel ( concat [
 
 -- * bass
 
+bassControl :: [Midi.Event Midi.Message] ;
 bassControl =
   controlCurve en brightnessCC
     (take 64 (cycle [0,31,62,93,124] ) ) ;
 
+bassNote :: Pitch -> [Midi.Event (Midi.Channel Midi.Message)] ;
 bassNote p = bassChannel ( concat [
   bass,
   controller volumeCC 100,
@@ -127,8 +144,10 @@ bassLoop =
 sweep =
    padChannel ( controlCurve en brightnessCC triangle) ;
 
+triangle :: [Integer] ;
 triangle = rampUp 0 ++ rampDown 124 ;
 
+rampUp, rampDown :: Integer -> [Integer] ;
 rampUp 124 = [] ;
 rampUp n = n : rampUp (n+4) ;
 
@@ -197,25 +216,30 @@ pattern3BassPad =
 
 -- * concatenation
 
+double :: [a] -> [a] ;
 double x = concat [ x, x ] ;
 
+quad :: [a] -> [a] ;
 quad x = concat [ x, x, x, x ] ;
 
+quadAlt :: [a] -> [a] -> [a] ;
 quadAlt x y = concat [ x, x, x, y] ;
 
 
 -- * durations
 
-en = 170 ;
-qn = 2 * en ;
-hn = 2 * qn ;
-wn = 2 * hn ;
-dwn = wn + hn ;
+en, qn, hn, wn, dwn, wn2 :: Midi.Time ;
+
+en  = 170 ;
+qn  = 2 * en ;
+hn  = 2 * qn ;
+wn  = 2 * hn ; dwn = wn + hn ;
 wn2 = 2 * wn ;
 
 
 -- * MIDI program
 
+ping, slap, pad, bass :: [Midi.Event Midi.Message] ;
 ping = program 0 ;
 slap = program 2 ;
 pad  = program 4 ;
@@ -224,6 +248,8 @@ bass = program 9 ;
 
 -- * MIDI channels
 
+soloChannel, patternChannel, padChannel, bassChannel
+   :: [Midi.Event a] -> [Midi.Event (Midi.Channel a)] ;
 soloChannel    xs = channel 0 xs ;
 patternChannel xs = channel 1 xs ;
 padChannel     xs = channel 2 xs ;
@@ -232,5 +258,6 @@ bassChannel    xs = channel 3 xs ;
 
 -- * MIDI controllers
 
+volumeCC, brightnessCC :: Midi.Controller ;
 volumeCC = 7 ;
 brightnessCC = 70 ;
