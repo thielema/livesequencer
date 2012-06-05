b30bb72de28e4ffa76dfb65f7c053861f424ecae
Henning Thielemann <git@henning-thielemann.de>
2012-06-05 13:00:22 +0200

    data/Enum: add Prelude import and signatures

--- data/base/Enum.hs
+++ data/base/Enum.hs
@@ -1,8 +1,12 @@
 module Enum where
 
+import Prelude (Eq, Num, (+), (-), ) ;
+
+succ, pred :: (Num a) => a -> a ;
 succ x = x+1 ;
 pred x = x-1 ;
 
+enumFrom, enumFromLazy :: (Eq a, Num a) => a -> [a] ;
 enumFrom 0 = enumFromLazy 0 ;
 enumFrom n = enumFromLazy n ;
 
