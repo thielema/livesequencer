10d09258595138b767dec14fccfd3b99f6d81bc2
Henning Thielemann <git@henning-thielemann.de>
2014-03-03 11:40:08 +0100

    Maybe.catMaybes, mapMaybe

--- data/base/Maybe.hs
+++ data/base/Maybe.hs
@@ -1,6 +1,8 @@
 module Maybe where
 
-import Function (id);
+import List (map);
+import ListLive (cons);
+import Function (id, ($), (.));
 import Prelude ();
 
 data Maybe a = Nothing | Just a;
@@ -12,3 +14,11 @@ maybe _ f (Just a) = f a;
 
 fromMaybe :: a -> Maybe a -> a;
 fromMaybe a = maybe a id;
+
+catMaybes :: [Maybe a] -> [a];
+catMaybes [] = [];
+catMaybes (mx : xs) =
+  maybe id cons mx $ catMaybes xs;
+
+mapMaybe :: (a -> Maybe b) -> [a] -> [b];
+mapMaybe f = catMaybes . map f;
